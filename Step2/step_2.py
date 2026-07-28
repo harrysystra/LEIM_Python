47a864353ea23471d7eabfaaa8f905fe60fb24db
MACKENZIE Harry <hmackenzie@systra.info>
2026-07-28 11:59:14 +0100

    added step2 inputs

---
+++ Step2/step_2.py
@@ -0,0 +1,16 @@
+# - read input files
+
+
+# - convert inputs from MSOA to ZonePfSH using proportions
+
+
+# - format data by scenario (core, high, low)
+
+
+# - aggregate data up to district level
+
+
+# - format outputs into correct structure for inputting to step 3
+
+
+# - output final csvs: 1_high-core, 2_low-core, 3_behavioural-core
